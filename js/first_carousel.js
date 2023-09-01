61af8881cf224e50231eb678ad622353249eba82
jichani <jichan7571@naver.com>
2023-09-01 13:51:10 +0900

    button까지 완료

--- js/first_carousel.js
+++ js/first_carousel.js
@@ -5,7 +5,7 @@ let first_swiper = new Swiper(".first_swiper", {
   effect: window.innerWidth > 769 ? 'fade' : '', // 페이드 효과 사용
   autoplay: {
     delay: 3000,
-    disableOnInteraction: false,
+    disableOnInteraction: true,
   },
   pagination: {
     el: ".swiper-pagination",

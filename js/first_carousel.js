0b8e5827c2eea7e6540cd13368526820bcbce2e1
jichani <jichan7571@naver.com>
2023-09-01 11:47:42 +0900

    버튼 구현 완료

--- js/first_carousel.js
+++ js/first_carousel.js
@@ -5,24 +5,31 @@ let first_swiper = new Swiper(".first_swiper", {
   effect: window.innerWidth > 769 ? 'fade' : '', // 페이드 효과 사용
   autoplay: {
     delay: 3000,
-    disableOnInteraction: true,
+    disableOnInteraction: false,
   },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    renderBullet: function (index, className) {
-      var textArray = ['재정관리', '주거관리', '진로관리', '건강관리', '법률관리'];
-      var svgArray = [
-        '<svg>...</svg>',  // 첫 번째 SVG 코드
-        '<svg>...</svg>',  // 두 번째 SVG 코드
-        '<svg>...</svg>',  // 세 번째 SVG 코드
-        '<svg>...</svg>',  // 네 번째 SVG 코드
-        '<svg>...</svg>'   // 다섯 번째 SVG 코드
-      ];
-      return '<span class="' + className + '">' +
-        svgArray[index] +
-        '<span>' + textArray[index] + '</span>' +
-        '</span>';
-    }
   },
+
+
+});
+
+var buttons = document.querySelectorAll('.carousel_button .button');
+buttons.forEach(function (button) {
+  button.addEventListener('click', function () {
+    var slideIndex = parseInt(this.getAttribute('data-slide'));
+    first_swiper.slideToLoop(slideIndex); // slideToLoop 메소드로 변경
+  });
+});
+
+first_swiper.on('slideChange', function () {
+  buttons.forEach(function (button) {
+    var slideIndex = parseInt(button.getAttribute('data-slide'));
+    if (slideIndex === first_swiper.realIndex) { // realIndex 속성으로 변경
+      button.classList.add('active'); // 'active'는 활성화 상태를 나타내는 클래스입니다.
+    } else {
+      button.classList.remove('active');
+    }
+  });
 });

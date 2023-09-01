cb3597c04a1f10a69dcf620a52e7113dbc3d268b
jichani <jichan7571@naver.com>
2023-09-01 10:25:58 +0900

    버튼 만들기 시작

--- js/first_carousel.js
+++ js/first_carousel.js
@@ -9,5 +9,20 @@ let first_swiper = new Swiper(".first_swiper", {
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      var textArray = ['재정관리', '주거관리', '진로관리', '건강관리', '법률관리'];
+      var svgArray = [
+        '<svg>...</svg>',  // 첫 번째 SVG 코드
+        '<svg>...</svg>',  // 두 번째 SVG 코드
+        '<svg>...</svg>',  // 세 번째 SVG 코드
+        '<svg>...</svg>',  // 네 번째 SVG 코드
+        '<svg>...</svg>'   // 다섯 번째 SVG 코드
+      ];
+      return '<span class="' + className + '">' +
+        svgArray[index] +
+        '<span>' + textArray[index] + '</span>' +
+        '</span>';
+    }
   },
 });

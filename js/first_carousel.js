1fe7e33a69547a75b3c3583766d34cf614b22faa
jichani <jichan7571@naver.com>
2023-09-04 13:44:03 +0900

    edit

--- js/first_carousel.js
+++ js/first_carousel.js
@@ -2,7 +2,7 @@ let first_swiper = new Swiper(".first_swiper", {
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
-  effect: window.innerWidth > 769 ? 'fade' : '', // 페이드 효과 사용
+  effect: 'fade', // 페이드 효과 사용
   autoplay: {
     delay: 3000,
     disableOnInteraction: true,

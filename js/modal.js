0de827c4d49334c2a3d7afc7cda9aab300b69038
jichani <jichan7571@naver.com>
2023-09-05 11:59:31 +0900

    edit

--- js/modal.js
+++ js/modal.js
@@ -2,12 +2,17 @@ const button = document.querySelector(".contact");
 const dialog = document.querySelector("dialog");
 const close_button = document.querySelector(".close_btn");
 
-
+const contact_buttons = document.querySelectorAll(".contact_button")
 
 button.addEventListener("click", () => {
   dialog.showModal();
-  AOS.init();
 });
+
+for (let i = 0; i < contact_buttons.length; i++) {
+  contact_buttons[i].addEventListener("click", () => {
+    dialog.showModal();
+  });
+}
 
 close_button.addEventListener("click", () => {
   dialog.close();

cd31c362999e583117d5ef5c277de60169e241d8
jichani <jichan7571@naver.com>
2023-09-06 16:58:51 +0900

    edit

--- js/form.js
+++ js/form.js
@@ -1,5 +1,7 @@
-const inform_buttons = document.querySelectorAll(".inqurey_option");
+let selectedValue; // 선택된 값 저장을 위한 전역 변수
 
+// 버튼에 이벤트 추가
+const inform_buttons = document.querySelectorAll(".inqurey_option");
 inform_buttons.forEach(button => {
   button.addEventListener("click", () => {
     // 모든 버튼에서 'checked' 클래스 제거
@@ -7,9 +9,7 @@ inform_buttons.forEach(button => {
     // 클릭한 버튼에만 'checked' 클래스 추가
     button.classList.add("checked");
     // 데이터 값 가져오기
-    const value = button.dataset.value;
-
-    console.log(value); // 콘솔에 값 출력
+    selectedValue = button.dataset.value;
   });
 });
 
@@ -17,9 +17,22 @@ inform_buttons.forEach(button => {
 const checkbox = document.getElementById("agreeCheckbox");
 const form_submit = document.getElementById("privateButton");
 
-form_submit.addEventListener("click", () => {
+form_submit.addEventListener("click", (e) => {
   if (!checkbox.checked) {
+    // 약관에 동의하지 않았다면 폼 제출 방지
     alert("약관에 동의하세요.");
-    e.preventDefault(); // 약관에 동의하지 않았다면 폼 제출 방지
+    e.preventDefault();
+  } else {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const call = document.getElementById("call").value;
+    const email = document.getElementById("email").value;
+    const text = document.getElementById("text").value;
+
+    console.log(`이름: ${name}`);
+    console.log(`번호: ${call}`);
+    console.log(`Email: ${email}`);
+    console.log(`버튼: ${selectedValue}`)
+    console.log(`텍스트: ${text}`);
   }
 });
--- js/toggle.js
+++ js/toggle.js
@@ -1,8 +1,6 @@
 const mainMenuTitle = document.querySelectorAll(".sideAccordion_title");
 const subMenues = document.querySelectorAll(".sideAccordion_menu");
 
-console.log(subMenues);
-
 for (let i = 0; i < mainMenuTitle.length; i++) {
   mainMenuTitle[i].addEventListener("click", () => {
     subMenues[i].classList.toggle("show");

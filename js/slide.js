101d02b60d4c92432deed2beed0de38d52b5321e
jichani <jichan7571@naver.com>
2023-09-06 15:30:48 +0900

    edit

--- js/slide.js
+++ js/slide.js
@@ -11,3 +11,10 @@ $(function () {
     e.preventDefault();
   });
 });
+
+$(function () {
+  $(".contact a").click(function (e) {
+    $.scrollTo(this.hash || 0, 500); /* 속도 절, 숫자가 작을수록 빠름 */
+    e.preventDefault();
+  });
+});

670b87869a85bb66ec95bd4740de402f4feda178
jichani <jichan7571@naver.com>
2023-09-07 16:41:10 +0900

    edit

--- js/slide.js
+++ js/slide.js
@@ -18,3 +18,10 @@ $(function () {
     e.preventDefault();
   });
 });
+
+$(function () {
+  $("header ui a").click(function (e) {
+    $.scrollTo(this.hash || 0, 500); /* 속도 절, 숫자가 작을수록 빠름 */
+    e.preventDefault();
+  });
+});
--- js/toggle.js
+++ js/toggle.js
@@ -1,13 +1,16 @@
-const mainMenuTitle = document.querySelectorAll(".sideAccordion_title");
-const subMenues = document.querySelectorAll(".sideAccordion_menu");
+const mainMenuItems = document.querySelectorAll(".sideAccordion_title");
 
-for (let i = 0; i < mainMenuTitle.length; i++) {
-  mainMenuTitle[i].addEventListener("click", () => {
-    subMenues[i].classList.toggle("show");
-    for (let j = 0; j < mainMenuTitle.length; j++) {
+for (let i = 0; i < mainMenuItems.length; i++) {
+  mainMenuItems[i].addEventListener("click", () => {
+
+    // 모든 메뉴 항목의 'show' 클래스 제거
+    for (let j = 0; j < mainMenuItems.length; j++) {
       if (i !== j) {
-        subMenues[j].classList.remove("show");
+        mainMenuItems[j].classList.remove("show");
       }
     }
+
+    // 현재 클릭한 메뉴 항목에 'show' 클래스 추가
+    mainMenuItems[i].classList.add("show");
   });
 }

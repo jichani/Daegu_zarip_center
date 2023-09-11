c120529eb98f5d7e0a6fe758cb37655dfb5a850b
jichani <jichan7571@naver.com>
2023-09-11 13:01:48 +0900

    일단 알림톡은 오는 거 확인!

--- js/solapi.js
+++ js/solapi.js
@@ -1,8 +1,6 @@
-require('dotenv').config();
-
 function btn_sendMessage_counselor() {
   let name = document.getElementById('name').value;
-  let tel = "01075400153";
+  let tel = "01086727571";
   let templateId = "KA01TP230907043126398rad96zOaFkv";
   let pfid = "KA01PF22041206411o33TFWW9Sl71Ppp";
 
@@ -30,8 +28,8 @@ function getAuthorization() {
   let salt = getSalt();
   let date = getDate();
   let value = date + salt;
-  let signature = getSignature(value, process.env.API_SEKRETret);
-  let authoriztion = 'HMAC-SHA256 apiKey=' + process.env.API_KEY + ', date=' + date + ', salt=' + salt + ', signature=' + signature;
+  let signature = getSignature(value, api_secret);
+  let authoriztion = 'HMAC-SHA256 apiKey=' + api_key + ', date=' + date + ', salt=' + salt + ', signature=' + signature;
   return authoriztion;
 }
 
@@ -223,44 +221,3 @@ function requestResult() {
     }
   }
 };
-
-let selectedValue; // 선택된 값 저장을 위한 전역 변수
-
-// 버튼에 이벤트 추가
-const inform_buttons = document.querySelectorAll(".inqurey_option");
-inform_buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    // 모든 버튼에서 'checked' 클래스 제거
-    inform_buttons.forEach(btn => btn.classList.remove("checked"));
-    // 클릭한 버튼에만 'checked' 클래스 추가
-    button.classList.add("checked");
-    // 데이터 값 가져오기
-    selectedValue = button.dataset.value;
-  });
-});
-
-function buttonConsulting(e) {
-  e.preventDefault();
-
-  // 선택된 버튼 찾기
-  const selectedButton = document.querySelector('.inqurey_option.checked');
-
-  // 만약 선택된 버튼이 없다면 경고 메시지 표시 후 함수 종료
-  if (!selectedButton) {
-    alert('문의할 카테고리를 선택해주세요.');
-    return;
-  }
-
-  // 선택된 버튼의 데이터 값 가져오기
-  const selectedValue = selectedButton.dataset.value;
-
-  console.log(selectedValue);
-
-  if (typeof API_KEY !== 'undefined' && typeof API_SEKRET !== 'undefined') {
-    // btn_sendMessage_customer();
-    // btn_sendMessage_counselor();
-    console.log("api 키는 있지만 일시적으로 막아둠!");
-  } else {
-    alert("api 키가 없으니 로컬에서만 작동합니다");
-  }
-}
